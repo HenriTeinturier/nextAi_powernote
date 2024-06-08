3f7a74c9958bda9eeef47dd8e112ced3eb8a3183
Teinturier <henri.teinturier@gmail.com>
2024-06-08 14:23:54 +0200

    first commit

---
+++ app/(dashboard-layout)/configuration/page.tsx
@@ -0,0 +1,30 @@
+import {Layout, LayoutDescription, LayoutHeader, LayoutTitle } from "@/features/page/layout";
+import { LayoutContent } from "@/features/page/layout";
+import { ConfigurationForm } from "./ConfigurationForm";
+import { requiredAuth } from "@/lib/auth/helper";
+import { prisma } from "@/lib/prisma";
+
+export default async function page(){
+    const user = await requiredAuth();
+
+    const configurations = await prisma.configuration.findMany({
+        where:{
+            userId: user.id,
+        }
+    })
+    return(
+        <Layout>
+            <LayoutHeader>
+            <LayoutTitle>
+                Edit Configuration
+            </LayoutTitle>
+            <LayoutDescription>
+                The configuration define the field you want to fill daily
+            </LayoutDescription>
+            </LayoutHeader>
+            <LayoutContent>
+                <ConfigurationForm  configurations={configurations}/>
+            </LayoutContent>
+        </Layout>
+    )
+}
